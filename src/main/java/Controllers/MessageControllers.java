30ff24ffb52b32c2550eedf2d64461a55d979869
Edouard <sibomanaedouard974@gmail.com>
2024-02-18 00:17:10 +0200

    about to finish

--- src/main/java/Controllers/MessageControllers.java
+++ src/main/java/Controllers/MessageControllers.java
@@ -1,9 +1,163 @@
+//package Controllers;
+//
+//import DbConfig.DbConnection;
+//import Models.Messages;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.annotation.WebFilter;
+//import jakarta.servlet.annotation.WebServlet;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.servlet.http.HttpSession;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//
+//@WebServlet("/sendMessage")
+//public class MessageControllers extends HttpServlet {
+//Connection connection= DbConnection.getConnection();
+//
+//    public MessageControllers() throws SQLException, ClassNotFoundException {
+//    }
+//
+//    public void init() throws ServletException {
+//    super.init();
+//    createMessageTable();
+//
+//}
+//    private void createMessageTable() {
+//        try {
+//            String query = "CREATE TABLE IF NOT EXISTS messages(" +
+//                    "id SERIAL PRIMARY KEY, " +
+//                    "message varchar(255) not null, " +
+//                    "receiver varchar(255) not null, " +
+//                    "sender varchar(255) not null)";
+//            PreparedStatement preparedStatement = connection.prepareStatement(query);
+//            int resultSet = preparedStatement.executeUpdate();
+//            if (resultSet > 0) {
+//                System.out.println("Table of messages was created successfully!");
+//            } else {
+//                System.out.println("Failed to create table!");
+//            }
+//            preparedStatement.close(); // Close the PreparedStatement
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
+//
+//    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        PrintWriter out=res.getWriter();
+//       HttpSession session=req.getSession();
+//    String message=req.getParameter("message");
+//    String sender = (String) session.getAttribute("id");
+//    String receiver=req.getParameter("receiver");
+//        Messages messages=new Messages();
+//        messages.setMessage(message);
+//        messages.setReceiver(receiver);
+//        messages.setSender(sender);
+//    try{
+//        PreparedStatement preparedStatement=connection.prepareStatement("INSERT INTO messages (message,receiver,sender) VALUES (?,?,?)");
+//        preparedStatement.setString(1,messages.getMessage());
+//        preparedStatement.setString(2,messages.getReceiver());
+//        preparedStatement.setString(3,messages.getSender());
+//        int resultSet= preparedStatement.executeUpdate();
+//        if(resultSet>0){
+//            out.println("Message sent successfully!");
+//
+//        }else {
+//            out.println("Failed to send message!");
+//        }
+//
+//    }catch (Exception e){
+//        System.out.println(e.getMessage());
+//    }
+//
+//
+//
+//    }
+//
+//
+//
+//    //this is the function to get messages
+//    public void getReceivedMessages(HttpServletRequest req,HttpServletResponse res) throws IOException {
+//        PrintWriter out =res.getWriter();
+//        res.setContentType("text/html");
+//        HttpSession session=req.getSession();
+//        String sender = (String) session.getAttribute("id");
+//        String receiver=req.getParameter("receiver");
+//        try{
+//            PreparedStatement preparedStatement=connection.prepareStatement("SELECT message FROM messages WHERE sender= ?  AND receiver= ?");
+//            preparedStatement.setString(1,sender);
+//            preparedStatement.setString(2,receiver);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//
+//            out.println("<ul>");
+//
+//            while (resultSet.next()) {
+//                String message = resultSet.getString("message");
+//                int messageId = resultSet.getInt("id");
+//                // Generate a link for each user
+//                out.println("<li><a href='Message.jsp?userId=" + messageId + "'>" + message + "</a></li>");
+//            }
+//
+//            out.println("</ul>");
+//            out.println("</body></html>");
+//
+//            resultSet.close();
+//            preparedStatement.close();
+//        }catch (Exception e){
+//            System.out.println(e.getMessage());
+//        }
+//
+//
+//
+//    }
+//
+//    public void getSentMessages(HttpServletRequest req, HttpServletResponse res) throws  IOException{
+//
+//        PrintWriter out =res.getWriter();
+//        res.setContentType("text/html");
+//        HttpSession session=req.getSession();
+//        String sender = (String) session.getAttribute("id");
+//        String receiver=req.getParameter("receiver");
+//        try{
+//            PreparedStatement preparedStatement=connection.prepareStatement("SELECT message FROM messages WHERE sender= ?  AND receiver= ?");
+//            preparedStatement.setString(1,receiver);
+//            preparedStatement.setString(2,sender);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//
+//            out.println("<ul>");
+//
+//            while (resultSet.next()) {
+//                String message = resultSet.getString("message");
+//                int messageId = resultSet.getInt("id");
+//                // Generate a link for each user
+//                out.println("<li><a href='Message.jsp?userId=" + messageId + "'>" + message + "</a></li>");
+//            }
+//
+//            out.println("</ul>");
+//            out.println("</body></html>");
+//
+//            resultSet.close();
+//            preparedStatement.close();
+//        }catch (Exception e){
+//            System.out.println(e.getMessage());
+//        }
+//
+//    }
+//
+//}
+
+
 package Controllers;
 
 import DbConfig.DbConnection;
 import Models.Messages;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,16 +173,17 @@ import java.sql.SQLException;
 
 @WebServlet("/sendMessage")
 public class MessageControllers extends HttpServlet {
-Connection connection= DbConnection.getConnection();
+    Connection connection;
 
-    public MessageControllers() throws SQLException, ClassNotFoundException {
+    public MessageControllers() {
+        try {
+            connection = DbConnection.getConnection();
+            createMessageTable();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void init() throws ServletException {
-    super.init();
-    createMessageTable();
-
-}
     private void createMessageTable() {
         try {
             String query = "CREATE TABLE IF NOT EXISTS messages(" +
@@ -44,39 +199,95 @@ Connection connection= DbConnection.getConnection();
                 System.out.println("Failed to create table!");
             }
             preparedStatement.close(); // Close the PreparedStatement
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
-    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        PrintWriter out=res.getWriter();
-       HttpSession session=req.getSession();;
-    String message=req.getParameter("message");
-    String sender = (String) session.getAttribute("id");
-    String receiver=req.getParameter("receiver");
-        Messages messages=new Messages();
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String action = req.getParameter("action");
+        if (action != null && !action.isEmpty()) {
+            switch (action) {
+                case "sendMessage":
+                    sendMessage(req, res);
+                    break;
+                case "viewSentMessages":
+                    getSentMessages(req, res);
+                    break;
+                case "viewReceivedMessages":
+                    getReceivedMessages(req, res);
+                    break;
+                default:
+                    res.getWriter().println("Invalid action");
+            }
+        }
+    }
+
+    private void sendMessage(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        HttpSession session = req.getSession();
+        String message = req.getParameter("message");
+        String sender = (String) session.getAttribute("id");
+        String receiver = req.getParameter("receiver");
+        Messages messages = new Messages();
         messages.setMessage(message);
         messages.setReceiver(receiver);
         messages.setSender(sender);
-    try{
-        PreparedStatement preparedStatement=connection.prepareStatement("INSERT INTO messages (message,receiver,sender) VALUES (?,?,?)");
-        preparedStatement.setString(1,messages.getMessage());
-        preparedStatement.setString(2,messages.getReceiver());
-        preparedStatement.setString(3,messages.getSender());
-        int resultSet= preparedStatement.executeUpdate();
-        if(resultSet>0){
-            out.println("Message sent successfully!");
-
-        }else {
-            out.println("Failed to send message!");
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO messages (message,receiver,sender) VALUES (?,?,?)");
+            preparedStatement.setString(1, messages.getMessage());
+            preparedStatement.setString(2, messages.getReceiver());
+            preparedStatement.setString(3, messages.getSender());
+            int resultSet = preparedStatement.executeUpdate();
+            if (resultSet > 0) {
+                out.println("Message sent successfully!");
+            } else {
+                out.println("Failed to send message!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    }catch (Exception e){
-        System.out.println(e.getMessage());
     }
 
-
-
+    private void getReceivedMessages(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        HttpSession session = req.getSession();
+        String sender = (String) session.getAttribute("id");
+        String receiver = req.getParameter("receiver");
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT message FROM messages WHERE sender= ?  AND receiver= ?");
+            preparedStatement.setString(1, sender);
+            preparedStatement.setString(2, receiver);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            displayMessages(out, resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    private void getSentMessages(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        PrintWriter out = res.getWriter();
+        HttpSession session = req.getSession();
+        String sender = (String) session.getAttribute("id");
+        String receiver = req.getParameter("receiver");
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT message FROM messages WHERE sender= ?  AND receiver= ?");
+            preparedStatement.setString(1, receiver);
+            preparedStatement.setString(2, sender);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            displayMessages(out, resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displayMessages(PrintWriter out, ResultSet resultSet) throws SQLException {
+        out.println("<ul>");
+        while (resultSet.next()) {
+            String message = resultSet.getString("message");
+            int messageId = resultSet.getInt("id");
+            out.println("<li><a href='Message.jsp?userId=" + messageId + "'>" + message + "</a></li>");
+        }
+        out.println("</ul>");
+    }
 }
--- src/main/webapp/Message.jsp
+++ src/main/webapp/Message.jsp
@@ -1,3 +1,39 @@
+<%--&lt;%&ndash;--%>
+<%--  Created by IntelliJ IDEA.--%>
+<%--  User: edouard--%>
+<%--  Date: 2024-02-17--%>
+<%--  Time: 15:41--%>
+<%--  To change this template use File | Settings | File Templates.--%>
+<%--&ndash;%&gt;--%>
+<%--<%@ page contentType="text/html;charset=UTF-8" language="java" %>--%>
+<%--<html>--%>
+<%--<head>--%>
+<%--    <title>Gosnarre</title>--%>
+<%--</head>--%>
+<%--<body>--%>
+<%--<h4>Talk with friends</h4>--%>
+
+<%--<form action="sendMessage" method="post">--%>
+<%--    <input type="hidden" name="receiver" value="<%= request.getParameter("userId") %>">--%>
+<%--    <label for="message">Message</label><br>--%>
+<%--    <textarea id="message" name="message" rows="4" cols="50" required></textarea><br>--%>
+<%--    <input type="submit" value="Send Message">--%>
+<%--</form>--%>
+
+<%--<form action="sendMessage" method="post">--%>
+<%--    <input type="hidden" name="receiver" value="<%= request.getParameter("userId") %>">--%>
+<%--    <button type="submit" name="sending">view sent messages</button>--%>
+<%--</form>--%>
+
+<%--<form action="sendMessage" method="post">--%>
+<%--    <input type="hidden" name="receiver" value="<%= request.getParameter("userId") %>">--%>
+<%--    <button type="submit" name="receiving" value="receiving">view received messages</button>--%>
+<%--</form>--%>
+
+
+<%--</body>--%>
+<%--</html>--%>
+
 <%--
   Created by IntelliJ IDEA.
   User: edouard
@@ -15,9 +51,21 @@
 
 <form action="sendMessage" method="post">
     <input type="hidden" name="receiver" value="<%= request.getParameter("userId") %>">
-    <label for="message">Message:</label><br>
+    <label for="message">Message</label><br>
     <textarea id="message" name="message" rows="4" cols="50" required></textarea><br>
-    <input type="submit" value="Send Message">
+    <input type="submit" name="action" value="sendMessage">
+</form>
+
+<form action="sendMessage" method="post">
+    <input type="hidden" name="receiver" value="<%= request.getParameter("userId") %>">
+    <input type="hidden" name="action" value="viewSentMessages">
+    <button type="submit">View Sent Messages</button>
+</form>
+
+<form action="sendMessage" method="post">
+    <input type="hidden" name="receiver" value="<%= request.getParameter("userId") %>">
+    <input type="hidden" name="action" value="viewReceivedMessages">
+    <button type="submit">View Received Messages</button>
 </form>
 
 </body>
